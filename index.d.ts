14676f95820a900d5b4ecf25efcaacb01bda554f
johannes-lindgren <johannes-lindgren@hotmail.com>
2022-09-05 16:17:30 +0200

    fix: removed unused index.d.ts.

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'storyblok-design-system/src/lib/internal-icons' {
-    type IconDef = {
-        viewBox: string
-        path: string
-    }
-    const icons: Record<string, IconDef | undefined>
-    export = icons
-}
