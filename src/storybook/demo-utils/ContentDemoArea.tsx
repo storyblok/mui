7b77535ac248d65997340af6d7cb45d41230ab5e
johannes-lindgren <johannes-lindgren@hotmail.com>
2022-10-10 21:11:41 +0200

    fix: Demo area using skeleton

--- src/storybook/demo-utils/ContentDemoArea.tsx
+++ src/storybook/demo-utils/ContentDemoArea.tsx
@@ -1,11 +1,11 @@
-import { Box } from '@mui/material'
+import { Skeleton } from '@mui/material'
 
 export const ContentDemoArea = () => (
-  <Box
-    sx={{
-      height: '500px',
-      background: (theme) => theme.palette.grey.A100,
-      borderRadius: 2,
-    }}
-  />
+  <>
+    <Skeleton
+      animation={false}
+      variant="rectangular"
+      height={500}
+    />
+  </>
 )
