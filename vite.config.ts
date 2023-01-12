a6b4bb3470a4a59f061a8075bbc6af2f9e6069c1
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-01-12 20:25:42 +0100

    EXT-946, EXT-947: Fix resolution of typescript aliases in export. Using Vite.

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import dts from 'vite-plugin-dts'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+export default defineConfig({
+  plugins: [
+    visualizer(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      name: 'storyblokMui',
+    },
+    rollupOptions: {
+      external: ['react', 'react-proptypes', 'react-dom', '@mui/material'],
+    },
+    emptyOutDir: false,
+  },
+  resolve: {
+    alias: {
+      '@src': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
